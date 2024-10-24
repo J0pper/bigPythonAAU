c78b2c5f9d6ccc223c6c904449724f6450ab9fb4
KneeCapStealer <87864377+KneeCapStealer@users.noreply.github.com>
2024-10-24 14:45:13 +0200

    THEY WILL KILL

--- main.py
+++ main.py
@@ -1,20 +1,66 @@
 import pygame as pg
 import time
-import threading as thread
-from gameobject import Projectile
+import random
+from gameobject import Projectile, GameObject
+
+
+def get_projectile_params() -> ((float, float), pg.Vector2):
+    direction = random.randint(1, 4)
+
+    match direction:
+        case 1:
+            direction = pg.Vector2(1, 0)
+
+        case 2:
+            direction = pg.Vector2(0, 1)
+
+        case 3:
+            direction = pg.Vector2(-1, 0)
+
+        case 4:
+            direction = pg.Vector2(0, -1)
+
+    xPos = 0
+    yPos = 0
+
+    if direction.x == 1:
+        xPos = -20
+
+    elif direction.x == -1:
+        xPos = mainSurface.get_size()[0] + 20
+
+    else:
+        xPos = random.randrange(10, mainSurface.get_size()[0] - 10)
+
+    if direction.y == 1:
+        yPos = -20
+
+    elif direction.y == -1:
+        yPos = mainSurface.get_size()[1] + 20
+
+    else:
+        yPos = random.randrange(10, mainSurface.get_size()[1] - 10)
+
+    return (xPos, yPos), direction
 
 
 def main():
-    projectile = Projectile((100, 100), pg.Vector2(0, 1))
+    Projectile(*get_projectile_params())
     startTime = time.time()
     prevTime = startTime
+    spawnerTimer = 0
 
     while True:
         deltaTime = time.time() - prevTime
         prevTime = time.time()
+        spawnerTimer += deltaTime
+        time.sleep(0.01)
+
+        if spawnerTimer > 5 / ((time.time() - startTime) / 4):
+            spawnerTimer = 0
+            Projectile(*get_projectile_params())
 
         Projectile.speed = (25 + 2 * (time.time() - startTime)) * deltaTime
-        print(Projectile.speed)
 
         for event in pg.event.get():
             if event.type == pg.KEYDOWN:
@@ -23,11 +69,13 @@ def main():
             elif event.type == pg.QUIT:
                 return
 
-        projectile.updatePos()
+        for proj in Projectile.projectiles:
+            proj.updatePos()
 
         # Rendering step
         mainSurface.fill((0, 255, 0))
-        projectile.draw(mainSurface)
+        for obj in GameObject.gameObjects:
+            obj.draw(mainSurface)
 
         move_pos()
         pg.display.flip()
