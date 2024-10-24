fd3512cd2070e79c4863488842f63e3a6ee631c8
Jeppe <Jeppe.vadandersen@gmail.com>
2024-10-24 12:39:44 +0200

    pygame settup

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import pygame as pg
+
+pg.init()
+mainSurface: pg.surface = pg.display.set_mode((1920, 1080), pg.FULLSCREEN, vsync=1)
+
 
 def main():
-    pass
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    return
+            elif event.type == pg.QUIT:
+                return
+
+        mainSurface.fill((255, 0, 0))
+        pg.display.flip()
+
 
 
 if __name__ == '__main__':
